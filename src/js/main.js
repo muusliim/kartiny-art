1d4da70bcaf5196a0173b1e0112f1cb19b05020e
Muslim <81385968+muslimon@users.noreply.github.com>
2023-06-26 16:44:54 +0300

    Slider is finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import modals from "./modules/modals";
+import slider from "./modules/sliders";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
     modals();
+    slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    slider('.main-slider-item', 'vertical');
 
 
 });
